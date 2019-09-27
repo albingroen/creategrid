4bae6edc60c5b6fe915a29e96cc6ebc8326b9634
Albin Groen <albin.groen@gmail.com>
2019-09-27 10:08:33 +0200

    🎉 Add component documentation

--- src/index.js
+++ src/index.js
@@ -1,26 +1,50 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-const CreateGrid = ({ items, minItemWidth, minItemHeight, gap, style }) => (
-  <div
-    style={{
-      display: "grid",
-      gridTemplateColumns: `repeat(auto-fit, minmax(${minItemWidth}, 1fr))`,
-      gridGap: gap,
-      ...style
-    }}
-  >
-    {items.map(item => (
+class CreateGrid extends Component {
+  static propTypes = {
+    /** An array of objects represnting a item in the grid. */
+    items: PropTypes.array,
+    /** A string with a value and a css suffix representing the mininum width of a grid-item. */
+    minItemWidth: PropTypes.string,
+    /** A string with a value and a css suffix representing the mininum height of a grid-item. */
+    minItemWidth: PropTypes.string,
+    /** A string with a value and a css suffix representing the gap between each item in the grid. */
+    gap: PropTypes.string,
+    /** A object of every possible json styling property */
+    style: PropTypes.object
+  };
+  static defaultProps = {
+    minItemHeight: "200px",
+    minItemWidth: "200px"
+  };
+
+  render() {
+    const { items, minItemWidth, minItemHeight, gap, style } = this.props;
+
+    return (
       <div
         style={{
-          background: item.background || "#eeeeee",
-          minHeight: minItemHeight,
-          gridRow: item.row || "auto",
-          gridColumn: item.col || "auto",
-          ...item.style
+          display: "grid",
+          gridTemplateColumns: `repeat(auto-fit, minmax(${minItemWidth}, 1fr))`,
+          gridGap: gap,
+          ...style
         }}
-      />
-    ))}
-  </div>
-);
+      >
+        {items.map(item => (
+          <div
+            style={{
+              background: item.background || "#eeeeee",
+              minHeight: minItemHeight,
+              gridRow: item.row || "auto",
+              gridColumn: item.col || "auto",
+              ...item.style
+            }}
+          />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default CreateGrid;
